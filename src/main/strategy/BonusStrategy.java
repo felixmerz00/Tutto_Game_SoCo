41c2c742832fb50ff16823a507a6641787c61b93
sugar-free55 <andre.seidenglanz@gmail.com>
2022-11-23 18:09:28 +0100

    added interface classes Bonus200, 300, 400, 500, 600

--- src/main/strategy/BonusStrategy.java
+++
@@ -1,5 +0,0 @@
-package strategy;
-
-public class BonusStrategy implements CardStrategyInterface{
-    //potentially create abstract class with individual bonuses as subclasses
-}
---
+++ src/main/strategy/BonusStrategy600.java
@@ -0,0 +1,5 @@
+package strategy;
+
+public class BonusStrategy600 implements CardStrategyInterface{
+    //consider create abstract class with individual bonuses as subclasses (BONUS IF TIME LEFT)
+}
