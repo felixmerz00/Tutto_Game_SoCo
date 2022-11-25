9d1049125b71f2e06c3207fea82cebd03f152798
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-11-25 11:46:19 +0100

    Tuple Klass erstellt. Verwendig i TurnLogic.

---
+++ src/main/Tuple.java
@@ -0,0 +1,16 @@
+public class Tuple {
+    /* This class exists, because tuples do not exist in Java. So the concrete strategy can
+    * return "new Tuple(0, false)" for example. In the TurnLogic I can then use this information.
+    *
+    * The TurnLogic calls a concrete Strategy which handles rolling dice. From this class
+    * TurnLogic needs the following Information:
+    * How many points were achieved? Because TurnLogic must update the score in the Player object.
+    * Did the player achieve Tutto? Because if he did, he can draw another card and roll the dice again. */
+    public int points;
+    public boolean achievedTutto;
+
+    public Tuple(int points, boolean achievedTutto){
+        this.points = points;
+        this.achievedTutto = achievedTutto;
+    }
+}
