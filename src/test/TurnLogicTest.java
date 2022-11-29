a38a2d3a6f3fd643914a7ed6452ec47aea72d653
Felix Merz <felixmerz00@gmail.com>
2022-11-29 10:06:46 +0100

    Test class created

---
+++ src/test/TurnLogicTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TurnLogicTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void playTurn() {
+    }
+
+    // Copy code from chapter 5.7 to test private method.
+    @Test
+    void playerWantsToContinue() {
+    }
+}
