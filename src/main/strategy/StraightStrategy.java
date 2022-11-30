7ddd4c8749217198f29b3bb7a53d0daed54a62dd
Timo Tietje <timolennard.tietje@uzh.ch>
2022-11-30 14:42:55 +0100

    Added an not fully worked out Version of StraightStrategy

--- src/main/strategy/StraightStrategy.java
+++ src/main/strategy/StraightStrategy.java
@@ -1,4 +1,34 @@
 package strategy;
 
-public class StraightStrategy implements CardStrategyInterface{
+//import dice.DiceCollection;
+
+public class StraightStrategy extends BaseStrategy{
+    //cant stop, new rule for is valid
+
+    @Override
+    protected String rollAgain(){
+        return "R";
+    }
+
+    @Override
+    protected boolean isValid(int Number, boolean triplet){
+        return false;//!aDiceCollection.hasIn(Number);
+    }
+
+    //mmh geht besser
+    /*public boolean isNull(DiceCollection aDiceCollection){
+        //wenn alle würfel in theDice auch in putBackDice sind.
+        return false;
+    }*/
+
+    @Override
+    public NullTuple executeStrategy() {
+        NullTuple result = super.executeStrategy();
+        if(result.success){
+            result.points = 2000;
+        }
+        else result.points = 0;
+
+        return result;
+    }
 }
