c921dae0c08097e21d58616328bc2107bd06b178
Mely <95911782+Meleac@users.noreply.github.com>
2022-12-05 16:34:48 +0100

    review warnings

--- src/main/cards/Bonus.java
+++ src/main/cards/Bonus.java
@@ -5,25 +5,9 @@ import strategy.*;
 public class Bonus extends Card {
     private final int bonusPoints;
 
-    /* check if bonusPoints = 200 or 300 or 400 or 500*/
-    /*private boolean validBonusPoints(int bonusPoints) {
-        if (bonusPoints != 200 || bonusPoints != 300 || bonusPoints != 400 || bonusPoints != 500 || bonusPoints == 600) {
-            return false;
-        } else {
-            return true;
-        }
-    }*/
-
-    /*not sure if check is needed for implementation... review when deck implementation is done*/
-
     public Bonus(int bonusPoints) {
         this.cardName = "Bonus";
         this.bonusPoints = bonusPoints;
-        /*if (validBonusPoints(bonusPoints)) {
-            this.bonusPoints = bonusPoints;
-        } else {
-            throw Error("Entered Bonus Points are Invalid");
-        }*/
 
         /*create instance of CardStrategy corresponding to this.bonusPoints*/
         if (this.bonusPoints == 200) {
